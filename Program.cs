4a441aa5b0ce45017c3b45c542681e619a6f8fef
Bryan Fidel Huaynalaya Rios <149826656+BryanFidel@users.noreply.github.com>
2024-02-21 21:09:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mi_primer_programa_de_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese el primer número:");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer número:");
+            double num3 = double.Parse(Console.ReadLine());
+
+            
+            double cuadrado = Math.Pow(num1, 2);
+            double raizCuadrada = Math.Sqrt(num2);
+            double raizCubica = Math.Pow(num3, 1.0 / 3.0);
+
+            
+            Console.WriteLine("El cuadrado de {0} es: {1}", num1, cuadrado);
+            Console.WriteLine("La raíz cuadrada de {0} es: {1}", num2, raizCuadrada);
+            Console.WriteLine("La raíz cúbica de {0} es: {1}", num3, raizCubica);
+
+            Console.ReadLine(); 
+        }
+    }
+}
+
