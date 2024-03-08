b5e7df8548f6b5c8bcfde8fe1ecd7929364ad0e0
Bryan Fidel Huaynalaya Rios <brayanfid@gmail.com>
2024-03-08 13:08:56 -0500

    solucion segundo algortimo

---
+++ Misegundoalgoritmo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Misegundoalgoritmo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 5;
+            int numero2 = 7;
+
+            // Sumar los números
+            int suma = numero1 + numero2;
+
+            // Mostrar el resultado
+            Console.WriteLine("La suma de " + numero1 + " y " + numero2 + " es " + suma);
+        }
+    }
+}
