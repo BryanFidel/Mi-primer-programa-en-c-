9e526658d2d4f6a23858e28dd34794a5d9e623a9
Bryan Fidel Huaynalaya Rios <brayanfid@gmail.com>
2024-03-08 20:12:44 -0500

    constantes

---
+++ Punto 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Punto_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const double sueldoBase = 2000;
+            const double porcentajeComision = 0.10;
+            const double objetivoVentas = 1000000;
+            const double beneficioExtra = 100000;
+        }
+    }
+}
